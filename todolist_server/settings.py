b4a612f26a569e3bdf1137fa1fa45fc24a2ba9d4
SunnyQjm <qjm253@gmail.com>
2018-09-12 15:38:07 +0800

    修复刷新token无效的bug

--- todolist_server/settings.py
+++ todolist_server/settings.py
@@ -153,7 +153,9 @@ REST_FRAMEWORK = {
 
 JWT_AUTH = {
     'JWT_EXPIRATION_DELTA': datetime.timedelta(hours=2),
+    'JWT_REFRESH_EXPIRATION_DELTA': datetime.timedelta(hours=2),
     'JWT_AUTH_HEADER_PREFIX': 'JWT',
+    'JWT_ALLOW_REFRESH': True,
 }
 
 # 跨域配置
