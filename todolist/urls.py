e2069f348872bcad48f9e0fb40cdd978d3bf9124
SunnyQjm <qjm253@gmail.com>
2018-09-12 09:09:02 +0800

    修改url

--- todolist/urls.py
+++ todolist/urls.py
@@ -4,10 +4,10 @@ from todolist import views
 
 urlpatterns = [
     # 获取待办事项列表
-    url(r'^tasklist/', views.getTaskList.as_view()),
+    url(r'^tasklist', views.getTaskList.as_view()),
     url(r'^addTask', views.addTask.as_view()),
     url(r'^register', views.Register.as_view()),
     url(r'^login', views.Login.as_view()),
     url(r'^updateToken', views.UpdateToken.as_view()),
-    url(r'^taskDetail/(?P<pk>[0-9]+)/$', views.TaskDetail.as_view()),
+    url(r'^taskDetail/(?P<pk>[0-9]+)$', views.TaskDetail.as_view()),
 ]
