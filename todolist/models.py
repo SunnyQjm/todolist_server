d2aec665e78a72b8016b19c0d8007bf64e8a18e2
SunnyQjm <qjm253@gmail.com>
2018-09-12 18:09:09 +0800

    时间的存储用时间戳来代替

--- todolist/models.py
+++ todolist/models.py
@@ -3,6 +3,10 @@ from __future__ import unicode_literals
 
 from django.db import models
 from django.utils import datetime_safe
+import time
+
+current_milli_time = lambda: int(round(time.time() * 1000))
+
 
 # Create your models here.
 
@@ -12,6 +16,6 @@ class Task(models.Model):
     tags = models.TextField(default='')
     priority = models.IntegerField(default=0)
     finished = models.BooleanField(default=False)
-    expire_date = models.DateTimeField(default=datetime_safe.datetime.now())
+    expire_date = models.CharField(max_length=15, default=current_milli_time())
     owner = models.ForeignKey('auth.User', related_name='task', on_delete=models.CASCADE)
 
